68705205bd40074af8f00ef5de6b4fe5411505e6
Brent Woodruff <brent@fprimex.com>
2014-10-01 23:58:43 -0400

    bump version for publishing.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.txt") as readme_file:
       long_description = readme_file.read()
 
 setup(name="zdeskcfg",
-      version="1.0.2",
+      version="1.0.3",
       description="Easy configuration of zendesk/zdesk scripts.",
       long_description=long_description,
       py_modules=["zdeskcfg"],
