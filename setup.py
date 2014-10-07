f4264751ee0d61831cee0b2d2138c22859549ea9
Brent Woodruff <brent@fprimex.com>
2014-10-07 12:19:35 -0400

    setup.py cleanup, bump version, push for
    supporting python 2 and python 3 without using 2to3 for conversion

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup
 import sys
 
-should_2to3 = sys.version >= '3'
-
 with open("README.txt") as readme_file:
       long_description = readme_file.read()
 
 setup(name="zdeskcfg",
-      version="1.0.3",
+      version="1.0.4",
       description="Easy configuration of zendesk/zdesk scripts.",
       long_description=long_description,
       py_modules=["zdeskcfg"],
-      classifiers=["Development Status :: 3 - Alpha",
+      classifiers=[
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: Software Development :: Libraries",
       ],
-      use_2to3=should_2to3,
       keywords="zendesk zdesk config",
       author="Brent Woodruff",
       author_email="brent@fprimex.com",
