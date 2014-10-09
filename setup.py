a7ce907a0a4d30ad8974ea0a2ffa153abc6c8526
Brent Woodruff <brent@fprimex.com>
2014-10-08 22:30:27 -0400

    bump version for update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.txt") as readme_file:
       long_description = readme_file.read()
 
 setup(name="zdeskcfg",
-      version="1.0.6",
+      version="1.0.7",
       description="Easy configuration of zendesk/zdesk scripts.",
       long_description=long_description,
       py_modules=["zdeskcfg"],
