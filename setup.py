15cbf310dd1f93b773874ce96347630269e71fa5
Brent Woodruff <brent@fprimex.com>
2018-08-24 11:27:07 -0400

    update version and dev status

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ with open("README.txt") as readme_file:
       long_description = readme_file.read()
 
 setup(name="zdeskcfg",
-      version="1.0.7",
+      version="1.1.0",
       description="Easy configuration of zendesk/zdesk scripts.",
       long_description=long_description,
       py_modules=["zdeskcfg"],
       classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Topic :: Software Development :: Libraries",
